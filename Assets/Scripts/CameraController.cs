33ce770f8dcceca9bb27f39cf5d108bee6aa114d
Phanty133 <vintersk@gmail.com>
2021-04-25 21:50:49 +0300

    Camera follows player if the camera doesnt fit the entire level

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -12,25 +12,44 @@ public class CameraController : MonoBehaviour
 	public LevelController levelController;
 	private float curDepth = 0f;
 	private PlayerController playerController;
+	private float minCamX;
+	private float maxCamX;
 
 	public void OnLevelViewChange(GameObject viewObj, int viewID, int direction){
 		transform.position = new Vector3(viewObj.transform.position.x, transform.position.y, transform.position.z);
 	}
 
 	private void UpdatePos(){
+		float newY = transform.position.y;
+		float newX = transform.position.x;
+
 		if(player.transform.position.y > -curDepth){ // If the player is above the theoretical depth of the camera, dont follow the player
-			transform.position = new Vector3(transform.position.x, -curDepth, transform.position.z);
+			newY = -curDepth;
 		}
 		else{
-			Vector2 curVel = new Vector2();
-			Vector2 dampedPos = Vector2.SmoothDamp(transform.position, (Vector2)player.transform.position + new Vector2(0, verticalOffsetFromPlayer), ref curVel, dampTime);
-		
-			transform.position = new Vector3(transform.position.x, dampedPos.y, transform.position.z);
+			newY = player.transform.position.y + verticalOffsetFromPlayer;
 		}
+	
+		if(player.transform.position.x > minCamX && player.transform.position.x < maxCamX){
+			newX = player.transform.position.x;
+		}
+
+		Vector2 curVel = new Vector2();
+		Vector2 dampedPos = Vector2.SmoothDamp(transform.position, new Vector2(newX, newY), ref curVel, dampTime);
+		transform.position = new Vector3(dampedPos.x, dampedPos.y, transform.position.z);
 	}
 
 	private void Awake() {
 		playerController = player.GetComponent<PlayerController>();
+
+		Camera camera = GetComponent<Camera>();
+		float unitWidth = camera.orthographicSize * camera.aspect * 2;
+
+		GameObject[] walls = GameObject.FindGameObjectsWithTag("LevelWall");
+		float levelWidth = Vector2.Distance(walls[0].transform.position, walls[1].transform.position);
+		float levelOverflow = (levelWidth - unitWidth) / 2 + 1f;
+		maxCamX = Mathf.Max(0, levelOverflow);
+		minCamX = -maxCamX;
 	}
 
 	private void LateUpdate() {
